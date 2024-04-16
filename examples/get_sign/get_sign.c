560b34bb625c0252b88fb77eb0153a73021b9b93
Hossein Monjezi <hossein.monjezi@live.com>
2024-04-16 02:41:25 +0200

    Fix get_sign example code

--- examples/get_sign/get_sign.c
+++ examples/get_sign/get_sign.c
@@ -2,32 +2,21 @@
  * First KLEE tutorial: testing a small function
  */
 
-#include <stdio.h>
 #include "klee/klee.h"
 
-int c = 0;
-
 int get_sign(int x) {
-  if (x == 0) {
-    int b = 0;
-    klee_subscribe_changing(&b, sizeof(b), "b_loc");
+  if (x == 0)
     return 0;
-  }
+
   if (x < 0)
     return -1;
   else
     return 1;
-}
+
+} 
 
 int main() {
-
   int a;
   klee_make_symbolic(&a, sizeof(a), "a");
-
-  int b = 0;
-  klee_subscribe_changing(&b, sizeof(b), "b");
-  klee_subscribe_changing(&c, sizeof(c), "c");
-
-  get_sign(a);
-  return 0;
+  return get_sign(a);
 }
